63f48c560ce0cf15e455bc97572fafcdc8a71d8b
Emilio Lopez <2023158951@ub.edu.bz>
2026-02-03 16:17:32 -0600

    Complete Part 1: Function declarations and expressions

---
+++ exercise1.js
@@ -0,0 +1,19 @@
+function calculateArea(length, width){
+	return length * width;
+}
+
+
+console.log(calculateArea(5,10));
+console.log(calculateArea(7, 3));
+console.log(calculateArea(4, 4));
+
+
+const calculatePerimeter = function(length, width){
+	return 2 * length + 2 * width;
+}
+
+console.log(calculatePerimeter(5, 10));
+console.log(calculatePerimeter(7, 3));
+console.log(calculatePerimeter(4,4));
+
+
